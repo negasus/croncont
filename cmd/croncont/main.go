187fcc68668ef1ee827a4769d454e0d7344d9531
Negasus <negasus@gmail.com>
2020-09-10 16:07:45 +0300

    add listen for metrics and health checker

--- cmd/croncont/main.go
+++ cmd/croncont/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"github.com/cristalhq/aconfig"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/robfig/cron/v3"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -21,11 +26,20 @@ type config struct {
 	Timeout        int    `default:"3000" usage:"request timeout, ms"`
 	ExpectedStatus int    `default:"200" usage:"expected response status code. 0 for no check"`
 
+	Listen string `default:"0.0.0.0:8001" usage:"listen address for healthcheck and metrics, disable if empty"`
+
 	Spec    string `default:"0 * * * * *" usage:"cron spec"`
 	Verbose bool   `default:"false" usage:"verbose mode"`
 }
 
 func main() {
+	var (
+		calls = promauto.NewCounter(prometheus.CounterOpts{
+			Name: "croncont_calls",
+			Help: "The total number of calls",
+		})
+	)
+
 	loader := aconfig.LoaderFor(&config{}).
 		WithEnvPrefix("CRON").
 		Build()
@@ -39,6 +53,16 @@ func main() {
 	if cfg.Verbose {
 		log.Printf("verbose mode on")
 	}
+
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	if cfg.Listen != "" {
+		go func() {
+			if err := listen(ctx, ctxCancel, cfg.Listen); err != nil {
+				log.Printf("error listen, %v", err)
+			}
+		}()
+	}
+	defer ctxCancel()
 
 	req, err := http.NewRequest(cfg.Method, cfg.URL, bytes.NewReader([]byte(cfg.Body)))
 	if err != nil {
@@ -67,6 +91,7 @@ func main() {
 	c := cron.New(cron.WithSeconds())
 
 	entryID, err := c.AddFunc(cfg.Spec, func() {
+		calls.Inc()
 		if cfg.Verbose {
 			log.Printf("Call %s %s", cfg.Method, cfg.URL)
 		}
@@ -100,9 +125,41 @@ func main() {
 	signal.Notify(ch, syscall.SIGTERM)
 	signal.Notify(ch, syscall.SIGINT)
 
-	<-ch
+	select {
+	case <-ch:
+		ctxCancel()
+	case <-ctx.Done():
+	}
 
 	c.Stop()
 
 	log.Printf("done")
 }
+
+func listen(ctx context.Context, ctxCancel context.CancelFunc, address string) error {
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/", livenessHandler)
+
+	server := &http.Server{
+		Handler: mux,
+	}
+
+	go func() {
+		log.Printf("listen %s", ln.Addr().String())
+		if err := server.Serve(ln); err != nil {
+			log.Printf("error serve, %v", err)
+			ctxCancel()
+		}
+	}()
+
+	<-ctx.Done()
+	return server.Shutdown(ctx)
+}
+
+func livenessHandler(_ http.ResponseWriter, _ *http.Request) {}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/cristalhq/aconfig v0.5.1
+	github.com/prometheus/client_golang v1.7.1
 	github.com/robfig/cron/v3 v3.0.0
 )
